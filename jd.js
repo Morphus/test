abcb2cee3547d0269e324cedb60e3ef53bacb9bd
Morphus <Morphus@users.noreply.github.com>
2022-01-01 00:00:00 +0800

    Initial commit

---
+++ jd.js
@@ -0,0 +1,3 @@
+var url = [
+"https://daojia.jd.com/taroh5/h5dist/#/pages/tabBar/signIn-t/index?channel=jdjryaoqianshu","https://u2.jr.jd.com/downloadApp/index.html?id=5878&activityId=3200","https://m.jr.jd.com/member/dailyAnswer/?channel=ttte"
+];
